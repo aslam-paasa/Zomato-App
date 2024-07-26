19a188874f25f5aa88e2f6201877969a06a8c516
Mohammad Aslam <36575542+aslam-paasa@users.noreply.github.com>
2024-07-27 01:33:26 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,36 @@
+/**
+ * Put everything inside HTTP Server and expose it to the world:
+*/
+
+import express from 'express';
+import dotenv from 'dotenv';
+
+/**
+ * Database Connection:
+*/
+import ConnectDB from './database/connection';
+
+dotenv.config();
+
+const zomato = express();
+
+zomato.use(express.json());
+
+zomato.get("/", (req, res) => {
+    res.json({
+        message: "Server is running!!!"
+    });
+});
+
+
+const PORT = 4000;
+zomato.listen(PORT, () => {
+    // ConnectDB()
+    //     .then(() => {
+    //         console.log("Server is running!!!");
+    //     })
+    //     .catch((error) => {
+    //         console.log("Server is running, but database connection failed...");
+    //     })
+    console.log("Server is running!!!");
+})
